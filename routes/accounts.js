1fea6f00791207fc404b8bf70e2c6ec1acb680cc
Hudson Tadeu <hudinvesting@gmail.com>
2022-08-23 20:53:22 -0300

    Adicionando método get

--- routes/accounts.js
+++ routes/accounts.js
@@ -8,14 +8,24 @@ const router = express.Router();
 router.post("/", async (req, res) => {
   try {
       let account = req.body;
-      const data = JSON.parse(await readFile("accounts.json"));
+      const data = JSON.parse(await readFile(global.fileName));
       
-      account = {id: data.nextId++, ...account} ;
+      account = {id: data.nextId++, ...account};
       data.accounts.push(account);
 
-      await writeFile("accounts.json", JSON.stringfy(data));
+      await writeFile(global.fileName, JSON.stringify(data, null, 2));
   
       res.send(account);
+  } catch (err) {
+      res.status(400).send({ error: err.message });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+      const data = JSON.parse(await readFile(global.fileName));
+      delete data.nextId; 
+      res.send(data);
   } catch (err) {
       res.status(400).send({ error: err.message });
   }
