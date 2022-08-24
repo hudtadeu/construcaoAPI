ec3fef39711454ce80d488677cf7901fb4daec9e
Hudson Tadeu <hudinvesting@gmail.com>
2022-08-24 17:46:22 -0300

    Adicionando método put e patch

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { promises as fs } from "fs";
+import { promises as fs, write } from "fs";
 
 const { readFile, writeFile } = fs;
 
@@ -49,6 +49,38 @@ router.delete("/:id", async (req, res) => {
       account => account.id !== parseInt(req.params.id));
     await writeFile(global.fileName, JSON.stringify(data, null, 2));
     res.end();
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.put("/", async (req, res) => {
+  try {
+    const account = req.body;
+    const data = JSON.parse(await readFile(global.fileName));
+    const index = data.accounts.findIndex(a => a.id === account.id);
+
+    data.accounts[index] = account;
+
+    await writeFile(global.fileName, JSON.stringify(data));
+
+    res.send(account);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.patch("/updateBalance", async (req, res) => {
+  try {
+    const account = req.body;
+    const data = JSON.parse(await readFile(global.fileName));
+    const index = data.accounts.findIndex(a => a.id === account.id);
+
+    data.accounts[index].balance = account.balance;
+
+    await writeFile(global.fileName, JSON.stringify(data));
+
+    res.send(data.accounts[index]);
   } catch (err) {
     res.status(400).send({ error: err.message });
   }
