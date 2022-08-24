663680583d2cb75e6b54124c682bf90b25507280
Hudson Tadeu <hudinvesting@gmail.com>
2022-08-24 10:48:02 -0300

    Adicionando complementação de get por id

--- routes/accounts.js
+++ routes/accounts.js
@@ -7,27 +7,38 @@ const router = express.Router();
 
 router.post("/", async (req, res) => {
   try {
-      let account = req.body;
-      const data = JSON.parse(await readFile(global.fileName));
-      
-      account = {id: data.nextId++, ...account};
-      data.accounts.push(account);
+    let account = req.body;
+    const data = JSON.parse(await readFile(global.fileName));
 
-      await writeFile(global.fileName, JSON.stringify(data, null, 2));
-  
-      res.send(account);
+    account = { id: data.nextId++, ...account };
+    data.accounts.push(account);
+
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+
+    res.send(account);
   } catch (err) {
-      res.status(400).send({ error: err.message });
+    res.status(400).send({ error: err.message });
   }
 });
 
 router.get("/", async (req, res) => {
   try {
-      const data = JSON.parse(await readFile(global.fileName));
-      delete data.nextId; 
-      res.send(data);
+    const data = JSON.parse(await readFile(global.fileName));
+    delete data.nextId;
+    res.send(data);
   } catch (err) {
-      res.status(400).send({ error: err.message });
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    const account = data.accounts.find(
+      account => account.id === parseInt(req.params.id));
+    res.send(account);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
   }
 });
 
